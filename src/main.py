d173d5f9d83f395e1957f0bca388d49c54dd2130
Dafydd James <dafydd@cantab.net>
2016-07-28 09:46:10 +0100

    Bind to all interfaces for Heroku.

--- src/main.py
+++ src/main.py
@@ -19,7 +19,7 @@ def load_config():
     return {
         'auth_token': os.getenv(ENVVAR_EVENTBRITE_PERSONAL_OAUTH_TOKEN),
         'event_id':   os.getenv(ENVVAR_EVENTBRITE_EVENT_ID),
-        'port':       os.getenv(ENVVAR_HEROKU_PORT, '5000'),
+        'port':       int(os.getenv(ENVVAR_HEROKU_PORT, '5000')),
         'base_url':   'https://www.eventbriteapi.com/v3'
     }
 
@@ -30,7 +30,7 @@ def run():
     # args = parse_args()
     client = eventbrite.EventbriteClient(config['auth_token'])
     # logging.debug(config)
-    app.run(port=config['port'])
+    app.run(host='0.0.0.0', port=config['port'])
     # report.print_click_report(client, args.eventId)
     # print simplejson.dumps(client.get_users_me())
     # print simplejson.dumps(client.get_users_me_owned_events())
